8a051b7c058355bdbeba8cb8adbe46dbd3988090
Hitesh Rinayat <hiteshrinayat321@gmail.com>
2024-12-12 14:20:09 +0530

    add the animation before the loading

--- public/js/videoSection.js
+++ public/js/videoSection.js
@@ -15,6 +15,22 @@ const nextButton = document.getElementById("nextButton");
 
 // Load videos for the current page
 function loadVideos(pageToken = "") {
+  // Show skeleton loader
+  videoContainer.innerHTML = `
+    <div class="skeleton-container">
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+      <div class="skeleton"><div class="skeleton-title"></div></div>
+    </div>
+  `;
+
   const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${encodeURIComponent(
     searchQuery
   )}&maxResults=${maxResults}&pageToken=${pageToken}&key=${apiKey}`;
@@ -22,7 +38,7 @@ function loadVideos(pageToken = "") {
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      videoContainer.innerHTML = ""; // Clear current videos
+      videoContainer.innerHTML = ""; // Clear skeletons after data loads
 
       if (data.items && data.items.length > 0) {
         data.items.forEach((item) => {
@@ -49,11 +65,7 @@ function loadVideos(pageToken = "") {
         prevPageToken = data.prevPageToken || ""; // If no previous page, reset
         pageNumberSpan.textContent = `Page ${currentPage}`;
       } else {
-        // If no videos found, loop back to the first page
-        currentPage = 1;
-        nextPageToken = "";
-        prevPageToken = "";
-        loadVideos(); // Reload the first page
+        videoContainer.innerHTML = "<p>No videos found.</p>";
       }
     })
     .catch((error) => {
@@ -68,10 +80,7 @@ nextButton.addEventListener("click", () => {
     currentPage++;
     loadVideos(nextPageToken);
   } else {
-    // Loop back to the first page if no next page
     currentPage = 1;
-    nextPageToken = "";
-    prevPageToken = "";
     loadVideos();
   }
 });
@@ -82,7 +91,6 @@ prevButton.addEventListener("click", () => {
     currentPage--;
     loadVideos(prevPageToken);
   } else {
-    // Loop back to the last page if no previous page (not directly possible in YouTube API)
     alert("You're on the first page!");
   }
 });
